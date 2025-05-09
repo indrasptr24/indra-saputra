c00a3bfa5438c50b1fbf2bcd82f0f89a1abd3b3c
indrasptr24 <saputr4ndra@gmail.com>
2025-05-10 03:22:11 +0700

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,3 +1,4 @@
+import streamlit as st
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing import image
 import numpy as np
@@ -26,3 +27,38 @@ def predict_hama(img_path, model, class_names):
     predicted_class = class_names[np.argmax(prediction)]
     confidence = np.max(prediction)
     return predicted_class, confidence
+
+st.title("Klasifikasi Hama Tanaman Padi")
+
+# File uploader untuk gambar
+uploaded_file = st.file_uploader("Upload gambar hama", type=["jpg", "jpeg", "png"])
+
+if uploaded_file is not None:
+    # Simpan file gambar sementara
+    img_path = os.path.join("images", uploaded_file.name)
+    with open(img_path, "wb") as f:
+        f.write(uploaded_file.getbuffer())
+
+    # Menampilkan gambar yang diupload
+    st.image(img_path, caption="Gambar yang diupload", use_column_width=True)
+
+    # Menyiapkan model dan memprediksi
+    model = load_hama_model()
+    class_names = ['Belalang','Penggerek_Batang','Tikus_Sawah','Ulat_Grayak','Walang_Sangit','Wereng_Coklat']
+solutions = {
+    'Belalang':  'Pengendalian biologis dengan burung pemangsa atau semprot insektisida kontak.',
+ 'Penggerek_Batang': 'Gunakan insektisida sistemik yang dapat diserap oleh tanaman serta lakukan pemangkasan pada bagian tanaman yang terinfeksi.',
+ 'Tikus_Sawah': 'Buat perangkap tikus atau gunakan umpan racun tikus yang aman dan Gunakan predator alami seperti kucing atau Burung Hantu.',
+ 'Ulat_Grayak': 'Gunakan insektisida yang efektif terhadap ulat, seperti insektisida berbahan aktif Bacillus thuringiensis (Bt).',
+ 'Walang_Sangit': 'Penggunaan insektisida kontak saat fase bunting hingga pengisian bulir.' ,
+ 'Wereng_Coklat' : 'Gunakan insektisida yang ditujukan untuk wereng, seperti insektisida berbahan aktif imidacloprid.'
+}
+    predicted_class, confidence = predict_hama(img_path, model, class_names)
+    
+    # Menampilkan hasil prediksi
+    st.write(f"Prediksi: {predicted_class}")
+    st.write(f"Confidence: {confidence * 100:.2f}%")
+    if class_label in solutions:
+        print(f"Solusi: {solutions[class_names]}")
+    else:
+        print("Solusi belum tersedia untuk kelas ini.")
