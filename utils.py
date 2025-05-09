c6682fae2c30d5445d563397f67363c8a3235d4b
indrasptr24 <saputr4ndra@gmail.com>
2025-05-10 02:28:51 +0700

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,12 +1,18 @@
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing import image
 import numpy as np
+import os
+import gdown
 
 def load_hama_model():
-    return load_model("model/model_hama.h5")
+    model_path = "model/model_hama.h5"
+    os.makedirs("model", exist_ok=True)
+        url = "https://drive.google.com/uc?id=1ahp6x3MDsBLz_M0vcRi6uq64XYlNb_TL" 
+        gdown.download(url, model_path, quiet=False)
+    return load_model(model_path)
 
 def predict_hama(img_path, model, class_names):
-    img = image.load_img(img_path, target_size=(224, 224))
+    img = image.load_img(img_path, target_size=(220, 220))
     img_array = image.img_to_array(img) / 255.0
     img_array = np.expand_dims(img_array, axis=0)
 
