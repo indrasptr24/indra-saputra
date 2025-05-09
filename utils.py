39438c68160aa1ec7707bb0d4822c96ce439245d
indrasptr24 <saputr4ndra@gmail.com>
2025-05-10 04:01:54 +0700

    Update utils.py

--- utils.py
+++ utils.py
@@ -35,6 +35,7 @@ uploaded_file = st.file_uploader("Upload gambar hama", type=["jpg", "jpeg", "png
 if uploaded_file is not None:
     # Simpan file gambar sementara
     os.makedirs("images", exist_ok=True)
+    
     img_path = os.path.join("images", uploaded_file.name)
     with open(img_path, "wb") as f:
         f.write(uploaded_file.getbuffer())
